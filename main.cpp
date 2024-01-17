cc62167aeeda914bc323ab89a9ecfc691c9da264
Robert Arifulin <robertarif2015@gmail.com>
2024-01-17 12:15:08 +0300

    firstCOM

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<map>
+#include<set>
+#include<algorithm>
+#include<string>
+#include<iterator>
+
+using namespace std;
+
+int main() {
+    int a;
+    cout << sizeof(a);
+    long aa;
+    cout << sizeof(aa);
+    long int aaa;
+    cout << sizeof(aaa);
+    long long aaaa;
+    cout << sizeof(aaaa);
+
+    return 0;
+}
